78d56e48fc1a7886ba37e9ac2ac3bfe80561fa27
huangjiuyuan <jiuyuan.huang@daocloud.io>
2018-05-06 23:48:51 +0800

    Chapter 5 section 5.1 done

---
+++ chapter-05/abstractclasses/Person.java
@@ -0,0 +1,15 @@
+package abstractclasses;
+
+public abstract class Person {
+    public abstract String getDescription();
+
+    private String name;
+
+    public Person(String n) {
+        name = n;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
