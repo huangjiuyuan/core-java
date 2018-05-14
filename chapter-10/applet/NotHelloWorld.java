364196d2f10fc2af33e832bf84c63850de3a3584
huangjiuyuan <jiuyuan.huang@daocloud.io>
2018-05-14 17:59:37 +0800

    Chapter 10 section 10.3 done

---
+++ chapter-10/applet/NotHelloWorld.java
@@ -0,0 +1,15 @@
+package applet;
+
+import java.awt.*;
+import javax.swing.*;
+
+public class NotHelloWorld extends JApplet {
+    public void init() {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                JLabel label = new JLabel("Not a Hello, World applet", SwingConstants.CENTER);
+                add(label);
+            }
+        });
+    }
+}
